7e9273929ef9110ecab9a29211ff3d3e3ab2f599
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-28 12:29:47 -0500

    Pass tests for failing when published recipe doesn't exist

--- src/Managers.RecipeManagementService/RecipeManagementService.cs
+++ src/Managers.RecipeManagementService/RecipeManagementService.cs
@@ -3,18 +3,31 @@
 public class RecipeManagementService
 {
     private readonly IRecipeEventNotifier notifier;
+    private readonly IRecipeAccess recipeAccess;
 
-    public RecipeManagementService(IRecipeEventNotifier notifier)
+    public RecipeManagementService(IRecipeEventNotifier notifier, IRecipeAccess recipeAccess)
     {
         this.notifier = notifier;
+        this.recipeAccess = recipeAccess;
     }
     
-    public void Publish(RecipeId recipeId){
+    public PublicationResult Publish(RecipeId recipeId){
+
+        var recipe = recipeAccess.FindRecipe(recipeId);
+        if (recipe == null) return new PublicationResult.UnknownRecipe(recipeId);
+
         notifier.Notify(RecipeEvent.Published, recipeId);
+
+        return new PublicationResult.Success();
     }
 }
 
 
+public record PublicationResult
+{
+    public record Success() : PublicationResult;
+    public record UnknownRecipe(RecipeId recipeId): PublicationResult;
+}
 
 public enum RecipeEvent{
     Published,
@@ -25,4 +38,21 @@ public interface IRecipeEventNotifier{
     void Notify(RecipeEvent recipeEvent, RecipeId recipeId);
 }
 
-public record RecipeId(Guid id);
+public record RecipeId(Guid id)
+{
+    public static RecipeId NewId()
+    {
+        return new RecipeId(Guid.NewGuid());
+    }
+};
+
+public record Markdown(string text);
+public record Recipe(
+    RecipeId Id,
+    string Title,
+    Markdown Instructions
+);
+public interface IRecipeAccess
+{
+    Recipe? FindRecipe(RecipeId id);
+}
--- test/RecipeManagementService.Tests/RecipeManagementServiceTests.cs
+++ test/RecipeManagementService.Tests/RecipeManagementServiceTests.cs
@@ -10,20 +10,43 @@ public class RecipeManagementServiceTests
 {
     RecipeManagementService sut;
     SpyRecipeEventNotifier spyNotifier;
+    InMemoryRecipeAccess recipeAccessDouble;
     public RecipeManagementServiceTests()
     {
         spyNotifier = new SpyRecipeEventNotifier();
-        sut = new RecipeManagementService(spyNotifier);
+        recipeAccessDouble = new InMemoryRecipeAccess();
+        sut = new RecipeManagementService(spyNotifier, recipeAccessDouble);
     }
 
     [Fact]
-    public void PublishRaisesNotification()
+    public void PublicationFailsIfRecipeIdIvalid()
     {
         RecipeId recipeId = new RecipeId(Guid.NewGuid());
+        var expectedResult = new PublicationResult.UnknownRecipe(recipeId);
 
-        sut.Publish(recipeId);
+        var actualResult = sut.Publish(recipeId);
 
-        var expectedEvent = new SpyRecipeEventNotifier.Notification(RecipeEvent.Published, recipeId);
+        Assert.Equal(expectedResult, actualResult);
+        Assert.Empty(spyNotifier.RecievedNotifications);
+    }
+
+    private Recipe GenerateRecipe()
+    {
+        return new Recipe(
+            RecipeId.NewId(),
+            "Sample",
+            new Markdown("Instructions")
+        );
+    }
+    [Fact]
+    public void PublishRaisesNotification()
+    {
+        Recipe recipe = GenerateRecipe();
+        recipeAccessDouble.CreateRecipe(recipe);
+
+        sut.Publish(recipe.Id);
+
+        var expectedEvent = new SpyRecipeEventNotifier.Notification(RecipeEvent.Published, recipe.Id);
         Assert.Equal(expectedEvent, spyNotifier.RecievedNotifications.FirstOrDefault());
     }
 }
@@ -38,5 +61,19 @@ class SpyRecipeEventNotifier : IRecipeEventNotifier
     public void Notify(RecipeEvent recipeEvent, RecipeId recipeId)
     {
         RecievedNotifications.Add(new Notification(recipeEvent, recipeId));
+    }
+}
+
+class InMemoryRecipeAccess : IRecipeAccess
+{
+    private List<Recipe> _recipes = new List<Recipe>();
+    public Recipe? FindRecipe(RecipeId id)
+    {
+        return _recipes.FirstOrDefault(r => r.Id == id);
+    }
+
+    public void CreateRecipe(Recipe recipe)
+    {
+        _recipes.Add(recipe);
     }
 }
