784f581b0ae86763c7d9d336afe8ee2cf3fa3fb9
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-28 11:12:37 -0500

    Add manager with basic notification

---
+++ src/Managers.RecipeManagementService/RecipeManagerService.cs
@@ -0,0 +1,26 @@
+﻿namespace Managers.RecipeManagementService;
+  
+public class RecipeManagerService
+{
+    private readonly IRecipeEventNotifier notifier;
+
+    public RecipeManagerService(IRecipeEventNotifier notifier)
+    {
+        this.notifier = notifier;
+    }
+    
+    public void Publish(RecipeId recipeId){
+        notifier.Notify(RecipeEvent.Published, recipeId);
+    }
+}
+
+public record RecipeId(Guid id);
+
+public enum RecipeEvent{
+    Published,
+    Unpublished
+}
+
+public interface IRecipeEventNotifier{
+    void Notify(RecipeEvent recipeEvent, RecipeId recipeId);
+}
