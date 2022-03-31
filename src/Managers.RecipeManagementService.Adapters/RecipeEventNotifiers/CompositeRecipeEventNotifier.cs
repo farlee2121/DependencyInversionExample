a67b28d6e363368bf69ee04094114805fd93d258
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-31 09:59:15 -0500

    Add a composite notifier type

---
+++ src/Managers.RecipeManagementService.Adapters/RecipeEventNotifiers/CompositeRecipeEventNotifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers.RecipeManagement.Adapters.RecipeEventNotifiers
+{
+    public class CompositeRecipeEventNotifier : IRecipeEventNotifier
+    {
+        private readonly IReadOnlyCollection<IRecipeEventNotifier> notifiers;
+
+        public CompositeRecipeEventNotifier(IReadOnlyCollection<IRecipeEventNotifier> notifiers)
+        {
+            this.notifiers = notifiers;
+        }
+
+        public void Notify(RecipeEvent recipeEvent, RecipeId recipeId)
+        {
+            foreach (var notifier in notifiers)
+            {
+                notifier.Notify(recipeEvent, recipeId);
+            }
+        }
+    }
+}
