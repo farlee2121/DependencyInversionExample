f223cc3a76a67031971453ad57aba3a80313d462
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-30 20:37:07 -0500

    Configure Authorized decorator for manager

---
+++ Managers.RecipeManagementService.Adapters/AuthorizedRecipeManagementServiceDecorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers.RecipeManagement.Identity
+{
+    public class AuthorizedRecipeManagementServiceDecorator : IRecipeManagementService
+    {
+        private readonly RequestContext requestContext;
+        private readonly IRecipeManagementService decorated;
+
+        
+        public AuthorizedRecipeManagementServiceDecorator(RequestContext requestContext, IRecipeManagementService decorated)
+        {
+            this.requestContext = requestContext;
+            this.decorated = decorated;
+        }
+        public void CreateRecipe(UserId userId, Recipe recipe)
+        {
+            if (userId != requestContext.CurrentUserId) throw new UnauthorizedAccessException();
+            
+            decorated.CreateRecipe(userId, recipe);
+        }
+
+        public IReadOnlyCollection<Recipe> ListRecipes(UserId userId)
+        {
+            if (userId != requestContext.CurrentUserId) throw new UnauthorizedAccessException();
+
+            return decorated.ListRecipes(userId);
+        }
+
+        public PublicationResult Publish(RecipeId recipeId)
+        {
+            return decorated.Publish(recipeId);
+        }
+
+        public Recipe? RecipeDetails(RecipeId recipeId)
+        {
+            return decorated.RecipeDetails(recipeId);
+        }
+
+
+        public record RequestContext(UserId? CurrentUserId);
+    }
+}
--- src/Clients.Web/CompositionRoot.cs
+++ src/Clients.Web/CompositionRoot.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.Extensions.DependencyInjection.Extensions;
 using Managers;
 using Managers.RecipeManagement;
+using Managers.RecipeManagement.Identity;
+using Clients.Web.Extensions;
+using static Managers.RecipeManagement.Identity.AuthorizedRecipeManagementServiceDecorator;
 
 namespace Clients.Web
 {
+#pragma warning disable CS8604 // Possible null reference argument.
     public class CompositionRoot
     {
 
         public static void RegisterAppServices(IServiceCollection services)
         {
-            services.AddScoped<IRecipeManagementService, RecipeManagementService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<RequestContext>(ctx =>
+            {
+                var httpContext = ctx.GetService<IHttpContextAccessor>();
+                return new RequestContext(httpContext?.HttpContext?.User.GetUserId());
+            });
+            services.AddScoped<IRecipeManagementService>(ctx =>
+            {
+                return new AuthorizedRecipeManagementServiceDecorator(
+                    ctx.GetService<RequestContext>(),
+                    new RecipeManagementService(ctx.GetService<IRecipeEventNotifier>(), ctx.GetService<IRecipeAccess>()));
+            });
             services.AddSingleton<IRecipeAccess, InMemoryRecipeAccess>();
             services.AddSingleton<IRecipeEventNotifier, NullRecipeEventNotifier>();
         }
     }
+#pragma warning restore CS8604 // Possible null reference argument.
 
 }
